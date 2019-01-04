b1a5fda78eb6566fac71b751c7606f4bb2eaafa0
ZS <dragon.200809@163.com>
2019-01-04 22:42:55 +0800

    maybe a bug in submodule

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 git clone -b multiverso-initial git@github.com:Microsoft/multiverso.git
 
+# vim multiverso/src/multiverso/communicator.cpp // change if (reg_info_.server_count >= 0 to if (reg_info_.server_count <= 0 in line 29
 cd multiverso
 cd third_party
 sh install.sh
